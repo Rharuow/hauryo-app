94ef7ee539cda7679a7e2ade7d88513db42a6729
Harysson <haryssonsoares@gmail.com>
2022-04-02 22:11:14 -0300

    feat: adjustiment sceen

--- components/Wellcome.tsx
+++ components/Wellcome.tsx
@@ -2,11 +2,12 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import TypeAnimation from "react-type-animation";
-import Emoji from 'a11y-react-emoji'
+import Emoji from "a11y-react-emoji";
 
 const Wellcome = () => {
   const [paragraph, setParagraph] = useState<number>(0);
   const [buttonText, setButtonText] = useState("Próxima");
+
   const content = {
     title: "Assim como a primavera, vc voltou!",
     body: [
@@ -31,93 +32,103 @@ const Wellcome = () => {
   };
 
   return (
-    <div className="h-100vh bg-dark text-light p-3">
+    <div className="h-100vh bg-dark text-light p-3 d-flex flex-wrap justify-content-center align-items-center">
       <h1 className="text-center">
         <TypeAnimation cursor={false} sequence={[content.title, 1000]} />
       </h1>
       <div className="d-flex w-100 justify-content-center">
         <img
-          src={`./stichhappy${paragraph <= 5 ? paragraph : paragraph === content.body.length - 1 ? 5 : Math.floor(Math.random() * 6)
-            }.png`}
+          src={`./stichhappy${
+            paragraph <= 5
+              ? paragraph
+              : paragraph === content.body.length - 1
+              ? 5
+              : Math.floor(Math.random() * 6)
+          }.png`}
           width={150}
           alt="stitch happy"
         />
       </div>
-      <p className="fs-4">
-        {paragraph === 0 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 1 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 2 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 3 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 4 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 5 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 6 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 7 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 8 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-        {paragraph === 9 && (
-          <TypeAnimation
-            cursor={true}
-            sequence={[1, content.body[paragraph]]}
-          />
-        )}
-      </p>
-      <div className="d-flex justify-content-between">
-        <Button
-          onClick={() => handleText()}
-          disabled={paragraph === content.body.length - 1}
-        >
-          {buttonText} {paragraph === content.body.length - 1 && <Emoji symbol="💕" label="love" />}
-        </Button>
+      <div className="w-100">
+        <p className="fs-4">
+          {paragraph === 0 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 1 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 2 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 3 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 4 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 5 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 6 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 7 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 8 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+          {paragraph === 9 && (
+            <TypeAnimation
+              cursor={true}
+              sequence={[1, content.body[paragraph]]}
+            />
+          )}
+        </p>
+      </div>
+      <div className="d-flex justify-content-around w-100">
         <Button
           variant="warning"
           onClick={() => backText()}
           disabled={paragraph === 0}
         >
           Anterior
+        </Button>
+        <Button
+          onClick={() => handleText()}
+          disabled={paragraph === content.body.length - 1}
+        >
+          {buttonText}{" "}
+          {paragraph === content.body.length - 1 && (
+            <Emoji symbol="💕" label="love" />
+          )}
         </Button>
       </div>
     </div>
